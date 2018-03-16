0b6d8bcc675d90a58558762dd359b955f9dc3ce1
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2018-03-16 11:51:38 +0200

    Major refactor & minor bug fixes
    TODO:
    -> colorize
    -> username
    -> handle interrupts better, terminate at end better

--- src.c
+++ src.c
@@ -6,147 +6,122 @@
 #include <sys/wait.h>
 #include  <signal.h>
 #include <sys/types.h>
-#define LINE_LENGTH 100 
+
+#define LINE_LENGTH 100
 #define HISTORY_LENGTH 100
 #define RHISTORY_LENGTH 10 //recent history for the "history" cmd
 
-const int ARGV_LENGTH = LINE_LENGTH/2+1;
+#define ARGV_LENGTH (LINE_LENGTH/2+1)
+
 int run;
 
+char history[HISTORY_LENGTH][LINE_LENGTH+5];
+int hi, hcnt;
+pid_t pid;
+
+void displayPrompt();
+
 //parse the line, sort arguments into argv
-void parseLine (char *line, char *argv[], int *cntp);
+void parseLine (char *line, char *argv[], int *argcntp);
 
 //frees the memory used to split and store the parameters
 void clearArgv(char *argv []);
 
-//kills all children who did not yet terminate 
-void killChildren ();
+int forkExec(char *args[], int *argcnt);
+
+//kills all children who did not yet terminate
+void interruptChildren ();
 
 void interruptHandler(int sig);
 
 int max(int a, int b);
+int min(int a, int b);
 
 void historize(int *hi, int *hcnt, char *src, char history[][LINE_LENGTH+5]);
+void printHistory();
+int historyNum(char *cmd);
+
 
 int main (){ //args later
 	signal(SIGINT, interruptHandler);
+
 	char *args[ARGV_LENGTH];
+	int argcnt;
 	char cmdline[LINE_LENGTH+5];
-	char cwd[LINE_LENGTH];
-	char history[HISTORY_LENGTH][LINE_LENGTH+5];
-	int child_status;
-	int background;
-	int cnt;
-	int hi, hcnt, hprev;
-	pid_t pid;
+
+	int execHistory, hprev;
 
 	run = 1;
 	hi = hcnt = 0;
+	execHistory = 0;
 	do {
-		background = 0;
-		hprev = -1;
 
-		getcwd(cwd, LINE_LENGTH);
-		printf("bashic $ (%s)> ", cwd);
-		fgets(cmdline, LINE_LENGTH, stdin);
-		parseLine(cmdline, args, &cnt);
+		displayPrompt();
+
+		char *src;
+		if (!execHistory){
+			fgets(cmdline, LINE_LENGTH, stdin);
+			src = cmdline;
+		} else{
+			src = history[hprev];
+			printf("%s", history[hprev]);
+			execHistory = 0;
+		}
+		parseLine(src, args, &argcnt);
+
 		waitpid(-1, NULL, WNOHANG); 		//to detect terminations in bg
 
-		if (cnt < 1)
+		if (argcnt < 1)
 			continue;
 
-		//commands not stored in history
-		if (!strcmp(args[0], "exit")){
+		//commands not stored in history:
+		int hnum;
+		hprev = -1;
+		if (strcmp(args[0], "exit") == 0)
 			run = 0;
-			continue;
-		} else if (!strcmp(args[0], "history")){
-			int i = hi-1, c = hcnt, j;
-			for (j = 0; j < RHISTORY_LENGTH && j < hcnt; j++){
-				if (i == -1)
-					i = HISTORY_LENGTH-1;
-				printf("%4d: %s", c, history[i]);
-				i--, c--;
+		else if (strcmp(args[0], "history") == 0)
+			printHistory();
+		else
+		   	if ((hnum = historyNum(args[0])) != -1){ // !! / !N
+			//1-based
+			if (hnum < max(hcnt-HISTORY_LENGTH+1, 1) || hnum > hcnt)
+				printf("Error executing a history command (invalid number)\n");
+			else{
+				hnum--; //0-based
+				hprev = hnum%HISTORY_LENGTH;
+				execHistory = 1;
 			}
-			continue;
-		} else if (!strcmp(args[0], "!!")){
-			if (hcnt == 0){
-				printf("Command history is empty...\n");
-				continue;
-			}
-			hprev = (hi == 0? HISTORY_LENGTH-1 : hi-1);
-			clearArgv(args);
-			parseLine(history[hprev], args, &cnt);
-		} else if (args[0][0] == '!'){
-			int all_dig = 0;
-			int n = strlen(args[0]);
-			char *numc = malloc(n);
-			strncpy(numc, args[0]+1, n); //to include '\0'
-			int i;
-			for (i = 0; i < n-1; i++)
-				if (!isdigit(numc[i])){
-					printf("Error executing a history command (invalid symbol)\n");
-					all_dig = 1;
-					break;
-				}
-			if (all_dig) continue;
-			int num = atoi(numc); //1-based
-			if (num < max(hcnt-HISTORY_LENGTH+1, 1) || num > hcnt){
-				printf("Error executing a history command (Command not in history or invalid number)\n");
-				continue;
-			}
-			num--; //now 0-based
-			hprev = num%HISTORY_LENGTH;
-			free(numc);
-			clearArgv(args);
-			parseLine(history[hprev], args, &cnt);
-		} 
-
-		//commands stored in history
-		char *src = cmdline;
-		if (hprev != -1){
-			src = history[hprev];
-			printf("> %s", history[hprev]);
 		}
-		historize(&hi, &hcnt, src, history);
+		//commands stored in history:
+		else {
+			historize(&hi, &hcnt, src, history);
 
-		if (!strcmp(args[0], "cd")){
-			if (chdir(args[1]))
-				printf("Error: Invalid path for cd\n");
-			continue;
-		}
-		
-		if (!strcmp(args[cnt-1], "&")){
-			free(args[cnt-1]);
-			args[cnt-1] = (char*)0;
-			background = 1;
-			cnt--;
-		}
-
-		pid = fork();
-		if (pid > 0){ //parent
-			if (!background){
-				waitpid(pid, &child_status, 0);
-				/*
-				if (WIFEXITED(child_status))
-					printf("(%s) terminated normally\n", args[0]);
-				*/
+			if (strcmp(args[0], "cd") == 0){
+				if (chdir(args[1]))
+					printf("Error: Invalid path for cd\n");
+			} else {
+				if (forkExec(args, &argcnt) == 0)
+					printf("Fork failed...\n");
 			}
-		} else if (pid == 0){ //child
-			if (execvp(args[0], args) == -1){
-				printf("Error executing..\n");
-				kill(getpid(), SIGKILL);
-			}
-		} else
-			printf ("Fork failed\n");
+		}
 
 	} while (clearArgv(args), run);
 
-	killChildren();
+	interruptChildren();
 	return 0;
 }
 
-void parseLine (char *line, char *argv[], int *cntp){
-   	char *s = line, *e = line; int i = 0;
+void displayPrompt(){
+	//later: colorize, username, etc
+	char cwd[LINE_LENGTH];
+	getcwd(cwd, LINE_LENGTH);
+
+	printf("bashic $ (%s)> ", cwd);
+}
+
+void parseLine (char *line, char *argv[], int *argcntp){
+   	char *s = line, *e = line;
+   	int i = 0;
 	while (i < ARGV_LENGTH-1){ //for safety
 
 		while (*e != '\0' && !isspace(*e)) e++;
@@ -168,7 +143,7 @@ void parseLine (char *line, char *argv[], int *cntp){
 			s = e = e+1;
 	}
 	argv[i] = (char *)0;	 //null pointer
-	*cntp = i;
+	*argcntp = i;
 }
 
 //frees the memory used to split and store the parameters
@@ -181,22 +156,97 @@ void clearArgv(char *argv []){
 	}
 }
 
-//kills all children who did not yet terminate 
-void killChildren (){
-	printf("Interrupting all children :(\n");
+int forkExec(char *args[], int *argcntp){
+	int argcnt = *argcntp;
+	int background = 0, child_status;
+
+	if (strcmp(args[argcnt-1], "&") == 0){
+		free(args[argcnt-1]);
+		args[argcnt-1] = (char*)0;
+		background = 1;
+		argcnt--;
+	}
+
+	pid = fork();
+	int ret = 1;
+	if (pid > 0){ //parent
+		if (!background)
+			waitpid(pid, &child_status, 0);
+	} else if (pid == 0){ //child
+		if (execvp(args[0], args) == -1){
+			printf("Error executing...\n");
+			kill(getpid(), SIGKILL);
+		}
+	} else{ 
+		ret = 0;
+		printf("Error forking...\n");
+	}
+
+	*argcntp = argcnt;
+	return ret;
+}
+
+//kills all children who did not yet terminate
+void interruptChildren (){
+	printf("Interrupting all children...\n");
 	kill(0, SIGINT);
 }
 
 void interruptHandler(int sig){
-	run = 0;
-	printf("\nExiting bashic:(\nPress any key to exit...\n");
+	if (pid == 0) //child
+		printf("\nChild interrupted\n");
+	else {
+		//run = 0;
+		printf("\n:(\n");
+	}
 }
+
 int max(int a, int b){
 	return a>b? a : b;
+}
+
+int min(int a, int b){
+	return a<b? a : b;
 }
 
 void historize(int *hi, int *hcnt, char *src, char history[][LINE_LENGTH+5]){
 	strncpy(history[*hi], src, strlen(src)+1);
 	*hi = (*hi+1)%HISTORY_LENGTH;
 	(*hcnt)++;
+}
+
+void printHistory(){
+	int i = hi-1, c = hcnt, j;
+	int rlength = min(RHISTORY_LENGTH, hcnt);
+	for (j = 0; j < rlength; j++){
+		if (i == -1)
+			i = HISTORY_LENGTH-1;
+		printf("%4d: %s", c, history[i]);
+		i--, c--;
+	}
+}
+
+int historyNum(char *cmd){
+	int num = -1;
+	if (strcmp(cmd, "!!") == 0)
+		num = hcnt;
+	else if (cmd[0] == '!'){
+		int n = strlen(cmd);
+		char *numc = malloc(n);
+		strncpy(numc, cmd+1, n); //to include '\0'
+		num = 1;
+
+		int i;
+		for (i = 0; i < n-1 && num; i++)
+			if (!isdigit(numc[i])) 
+				num = -1;
+		if (num)
+			num = atoi(numc);
+
+		free(numc);
+	}
+
+	return num;
+	//0: not a history cmd
+	//value of num
 }
